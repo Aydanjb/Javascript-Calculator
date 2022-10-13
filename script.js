eea915b40c988188710a9de1949364b5d7a6f367
Aydan <91035120+Aydanjb@users.noreply.github.com>
2022-10-13 19:19:14 -0400

    Created variables

---
+++ script.js
@@ -0,0 +1,16 @@
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+const numButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector("#equals");
+const decimalButton = document.querySelector("#decimal");
+const previousDisplay = document.querySelector("#previousDisplay");
+const currentDisplay = document.querySelector("#currentDisplay");
+
+numButtons.forEach(button => {
+    
+});
+
+operatorButtons.forEach(button => {
+    
+});
