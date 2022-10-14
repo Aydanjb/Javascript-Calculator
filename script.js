24f7531e850b442f3bbb4558e52cd43a6485b205
Aydan <91035120+Aydanjb@users.noreply.github.com>
2022-10-14 11:39:35 -0400

    numberButton and clear functionality

--- script.js
+++ script.js
@@ -7,14 +7,58 @@ const decimalButton = document.querySelector("#decimal");
 const previousDisplay = document.querySelector("#previousDisplay");
 const currentDisplay = document.querySelector("#currentDisplay");
 
+let operandOne = "";
+let operandTwo = "";
+let operation = "";
+
 const clear = () => {
+    operandOne = "";
+    operandTwo = "";
+    operation = "";
+    currentDisplay.textContent = "";
+    previousDisplay.textContent = "";
+};
+
+const backspace = () => {
 
 };
 
+const pushNumber = (value) => {
+    if (value === "." && operandOne.includes(".")) return;
+    operandOne += value;
+};
+
+
+
+const updatePreviousDisplay = (value) => {
+    if (operandOne === "") return;
+    if (operandTwo !== "") {
+        evaluate();
+    }
+    operation = value;
+    operandTwo = operandOne;
+    operandOne = "";
+};
+
+const updateDisplay = () => {
+    currentDisplay.textContent = operandOne;
+    previousDisplay.textContent = operandTwo;
+};
+
+clearButton.addEventListener("click", () => {
+    clear();
+})
+
 numButtons.forEach(button => {
-    
+    button.addEventListener("click", () => {
+        pushNumber(button.textContent);
+        updateDisplay();
+    })
 });
 
 operatorButtons.forEach(button => {
-    
+    button.addEventListener("click", () => {
+        updatePreviousDisplay(button.textContent);
+        updateDisplay();
+    })
 });
